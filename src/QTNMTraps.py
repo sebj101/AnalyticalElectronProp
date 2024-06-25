31b5b03cec6351326e1129c10864f5b717c76944
Seb Jones <jonesseb100@gmail.com>
2024-06-25 12:47:37 +0100

    Created BathtubTrap class

--- src/QTNMTraps.py
+++ src/QTNMTraps.py
@@ -7,6 +7,7 @@ Implementations:
 ---------------
 
 HarmonicTrap: Harmonic magnetic field providing trapping field 
+BathtubTrap: Two-coil bathtub magnetic field providing trapping field
 """
 
 import numpy as np
@@ -123,3 +124,121 @@ class HarmonicTrap(BaseTrap):
         zMax = self.CalcZMax(pitchAngle)
         omegaA = self.CalcOmegaAxial(pitchAngle, v)
         return zMax * np.sin(omegaA * t)
+
+
+class BathtubTrap(BaseTrap):
+    """
+    Trap class representing a bathtub magnetic field
+    """
+
+    __B0 = 0.0
+    __L0 = 0.0
+    __L1 = 0.0
+
+    def __init__(self, B0, L0, L1):
+        """
+        Constructor for BathtubTrap
+        """
+        self.__B0 = B0
+        self.__L0 = L0
+        self.__L1 = L1
+
+    def CalcZMax(self, pitchAngle):
+        """
+        Calc the maximum axial position
+
+        Parameters:
+        ----------
+        pitchAngle: float representing the pitch angle in radians
+        """
+        return self.__L0 / np.tan(pitchAngle)
+
+    def CalcOmegaAxial(self, pitchAngle, v):
+        """
+        Get the axial frequency of the electron's motion in radians/s
+
+        Parameters:
+        ----------
+        pitchAngle: float representing the pitch angle in radians
+        v: float representing the speed of the electron in m/s
+        """
+        wa = v * np.sin(pitchAngle) / self.__L0
+        return wa / (1 + self.__L1 * np.tan(pitchAngle) / (self.__L0 * np.pi))
+
+    def CalcOmega0(self, v, pitchAngle):
+        """
+        Get the average cyclotron frequency in radians/s
+
+        Parameters:
+        ----------
+        v: float representing the speed of the electron in m/s
+        pitchAngle: float representing the pitch angle in radians
+        """
+        beta = v / sc.c
+        gamma = 1 / np.sqrt(1 - beta**2)
+        prefac = sc.e * self.__B0 / (sc.m_e * gamma)
+        return prefac * (1 + (self.CalcZMax(pitchAngle)**2 / (2 * self.__L0**2)) / (1 + self.__L1 * np.tan(pitchAngle) / (self.__L0 * np.pi)))
+
+    def GetBzTime(self, t, pitchAngle, v):
+        """
+        Get the axial component of the magnetic field at a given time
+
+        Parameters:
+        ----------
+        t: float representing the time in seconds
+        pitchAngle: float representing the pitch angle in radians
+        v: float representing the speed of the electron in m/s
+        """
+        zMax = self.CalcZMax(pitchAngle)
+        wa = v * np.sin(pitchAngle) / self.__L0
+        t0 = 0.0
+        t1 = self.__L1 / (v * np.cos(pitchAngle))
+        t2 = t1 + np.pi / wa
+        t3 = t1 + t2
+        T = 2 * t2
+
+        tPeriodic = t % T
+        if tPeriodic < t1:
+            return self.__B0
+        elif tPeriodic > t1 and tPeriodic < t2:
+            return self.__B0 * (1 + zMax**2 / (2 * self.__L0**2) - zMax**2 / (2 * self.__L0**2) * np.cos(2 * wa * (tPeriodic - t1)))
+        elif tPeriodic > t2 and tPeriodic < t3:
+            return self.__B0
+        else:
+            return self.__B0 * (1 + zMax**2 / (2 * self.__L0**2) - zMax**2 / (2 * self.__L0**2) * np.cos(2 * wa * (tPeriodic - t3)))
+
+    def GetBzPosition(self, pos):
+        """
+        Get the axial component of the magnetic field at a given position
+
+        Parameters:
+        ----------
+        pos: numpy array representing the position in metres
+        """
+        z = pos[2]
+        if z < -self.__L1 / 2:
+            return self.__B0 * (1 + (z + self.__L1 / 2)**2 / self.__L0**2)
+        elif z > -self.__L1 / 2 and z < self.__L1 / 2:
+            return self.__B0
+        else:
+            return self.__B0 * (1 + (z - self.__L1 / 2)**2 / self.__L0**2)
+
+    def GetZPosTime(self, t, v, pitchAngle):
+        zMax = self.CalcZMax(pitchAngle)
+        wa = v * np.sin(pitchAngle) / self.__L0
+        t0 = 0.0
+        t1 = self.__L1 / (v * np.cos(pitchAngle))
+        t2 = t1 + np.pi / wa
+        t3 = t1 + t2
+        T = 2 * t2
+        vz0 = v * np.cos(pitchAngle)
+
+        tPeriodic = t % T
+        if tPeriodic < t1:
+            return vz0 * tPeriodic - self.__L1 / 2
+        elif tPeriodic > t1 and tPeriodic < t2:
+            return zMax * np.sin(wa * (tPeriodic - t1)) + self.__L1 / 2
+        elif tPeriodic > t2 and tPeriodic < t3:
+            return -vz0 * (tPeriodic - t2) + self._L1 / 2
+        else:
+            return -zMax * np.sin(wa * (tPeriodic - t3)) - self.__L1 / 2
